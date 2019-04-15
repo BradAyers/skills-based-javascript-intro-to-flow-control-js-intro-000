a3545c3c68d3113b5cb6c5ef990d3f58a88c6ecc
Brad Ayers <brad@bradayers.com>
2019-04-15 23:21:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,6 +3,12 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
+  if (age>12 && age< 20) {
+    return "You are a teenager!";
+  }
+  else {
+    return "You are not a teenager";
+  }
 
 }
 
