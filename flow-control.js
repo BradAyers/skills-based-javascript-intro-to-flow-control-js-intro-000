b382576521795f1282cc83829d10ecaf9fd58576
Brad Ayers <brad@bradayers.com>
2019-04-15 23:41:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,5 +28,30 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+var teenager = "You are a teenager!";
+switch (age) {
+  case 13 : 
+  console.log(teenager);
+  break;
+  case 14 : 
+  console.log(teenager);
+  break;
+  case 15 : 
+  console.log(teenager);
+  break;
+  case 16 : 
+  console.log(teenager);
+  break;
+  case 17 : 
+  console.log(teenager);
+  break;
+  case 18 : 
+  console.log(teenager);
+  break;
+  case 19 : 
+  console.log(teenager);
+  break;
+  default :
+  console.log("You have an age");
+}
 }
